2e8d55eca48779c851ca6737df6112bb2fdfbcf9
Nitoo kumari <160069703+nitoo124@users.noreply.github.com>
2024-04-27 13:22:37 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+const input_box = document.getElementById('input-box');
+
+const list_container = document.getElementById('list-container');
+
+function AddTask() {
+    if (input_box.value === '') {
+
+        alert('You must write something!')
+        
+    }else{
+        let li = document.createElement('li');
+        li.innerHTML = input_box.value;
+        list_container.appendChild(li);
+        let span  = document.createElement('span')
+        span.innerHTML = '\u00d7';
+        li.appendChild(span);
+    }
+    input_box.value = '';
+    save_data();
+
+}
+
+list_container.addEventListener('click', function(e){
+    if (e.target.tagName === 'LI') {
+        e.target.classList.toggle('checked');
+        save_data();
+    }
+    else if(e.target.tagName === 'SPAN'){
+        e.target.parentElement.remove();
+        save_data();
+    }
+}, false);
+
+function save_data(){
+    localStorage.setItem('data', list_container.innerHTML);
+}
+
+function show_task(){
+    list_container.innerHTML = localStorage.getItem('data')
+}
+show_task();
